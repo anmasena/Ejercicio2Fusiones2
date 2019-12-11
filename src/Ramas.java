9f575c5fc9968c26e6750cbc8750fd6949607349
anmasena <anmasena@gmail.com>
2019-12-11 11:08:06 +0100

    Rectificado los errores

--- src/Ramas.java
+++ src/Ramas.java
@@ -2,12 +2,11 @@
 public class Ramas {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int a = 1;
 		int d = 5;
 		System.out.println(a);
 		System.out.println(d);
-=======
 		int a = 1; // creado en la rama master
 		int b = 2; //creado en la rama testing
 		int c = 3; //creado en la rama testing en el siguiente commit
@@ -16,7 +15,6 @@ public class Ramas {
 		System.out.println(b);
 		System.out.println(c);
 
->>>>>>> testing
 	}
 
 }
