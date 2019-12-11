cfe25e701b26f43cc72f6dee24cccd524aba0bc6
anmasena <anmasena@gmail.com>
2019-12-11 10:59:38 +0100

    Creada la variable b en la rama testing

--- src/Ramas.java
+++ src/Ramas.java
@@ -2,8 +2,10 @@
 public class Ramas {
 
 	public static void main(String[] args) {
-		int a = 1;
+		int a = 1; // creado en la rama master
+		int b = 2; //creado en la rama testing
 		System.out.println(a);
+		System.out.println(b);
 
 	}
 
