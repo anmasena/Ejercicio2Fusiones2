8097e120276c46023d60fd412283d7fc5f64bc7f
anmasena <anmasena@gmail.com>
2019-12-11 11:01:21 +0100

    creada la variable c  en la rama testing

--- src/Ramas.java
+++ src/Ramas.java
@@ -4,8 +4,11 @@ public class Ramas {
 	public static void main(String[] args) {
 		int a = 1; // creado en la rama master
 		int b = 2; //creado en la rama testing
+		int c = 3; //creado en la rama testing en el siguiente commit
+
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
 
 	}
 
